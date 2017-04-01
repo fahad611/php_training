5c34a346fe711bbd9e18095d23498074135f5d61
Your Name <you@example.com>
2017-04-01 12:57:04 -0700

    Data types

---
+++ data_types.php
@@ -0,0 +1,24 @@
+<?php
+	//string data type
+
+	$name = "fahad";
+
+	//integer data type
+
+	$number = 123;
+
+	//boolean data type
+
+	$value1 = true;
+	$value2 = false;
+
+	if ($value1 == false) {
+		echo "this is true";
+	}
+	else{
+		echo "this value is false";
+	}
+
+
+
+?>
