1d06d7a2b18f9e2efb61deda76d138432b07232b
Your Name <you@example.com>
2017-04-01 12:55:14 -0700

    Arrays

---
+++ arrays.php
@@ -0,0 +1,11 @@
+<?php
+
+$info = array("fahad","age","email@gmail.com","karachi","paksitani","male");
+
+echo "Name of the student is:".$info[0]."<br> age of student:".$info[1]."<br>email id of student:".$info[2].    "<br> city of student:".$info[3]."<br> nationality of student:".$info[4]."<br>gender of student:".$info[5];
+
+
+
+
+
+?>
