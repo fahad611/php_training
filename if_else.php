c960303ae2eed6f3f0496198f9c9871a2d4a5a3d
Your Name <you@example.com>
2017-04-01 13:04:34 -0700

    Conditional Statements

---
+++ if_else.php
@@ -0,0 +1,31 @@
+<?php
+
+    $country = "turkey";
+
+	if($country=="pakistan") {
+
+	echo "pakistan is beautiful country";
+
+	}
+	if ($country=="turkey") {
+		echo "turkey is beautiful country";
+	}
+	else{
+		echo "this country is not pakistan";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
