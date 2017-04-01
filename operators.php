e65ebcd95824002deaa9d067f902f1cb1d0d882d
Your Name <you@example.com>
2017-04-01 13:06:51 -0700

    Operators use in php

---
+++ operators.php
@@ -0,0 +1,50 @@
+<?php
+		//Arthematic operators
+		
+		$name = 23 + 10;
+		echo $name;
+
+		//Asignment operators
+
+		$number =  23;
+
+		echo $number;
+
+
+		// incrementing and decrementing operators
+
+		$num = 33;
+
+		$num++;
+		echo $num;
+
+		//comparison operators
+
+		$num1 = 33;
+		$num2 = 43;
+
+		if($num1<$num2){
+
+			echo "variables are equal";
+		}
+		else{
+			echo "variables are not equal";
+		}
+
+		//logical operator
+
+		$num3 = 50;
+		$num4 = 60;
+
+		if ($num3 == $num4 OR $num3 < $num4) {
+			echo "anyone condtion is true";
+		}
+		else{
+			echo "no one is true";
+		}
+
+
+
+
+
+?>
