6e06661e08fa4baecbf8e023d39fb8e37b11cacf
Your Name <you@example.com>
2017-04-01 13:10:16 -0700

    Using variable

---
+++ variable.php
@@ -0,0 +1,22 @@
+<?php
+ $name = "Fahad Rashid";
+ $age = 24;
+ $email = "asd@gmail.com";
+
+ $result = "$name$age$email";
+
+ $age1 = 24;
+ $age2 = 24;
+
+ 
+
+ $result1 = $age1 + $age2;
+
+ echo "$result1<br>";
+
+ echo "$result<br>";
+
+ echo "your name is:".$name."<br>your age is:".$age."<br>yoir email id is:".$email;
+
+
+?>
