5207c734b230a2f606c1be498c89a3f57369215e
Your Name <you@example.com>
2017-04-01 13:05:49 -0700

    Using loops

---
+++ loops.php
@@ -0,0 +1,38 @@
+<?php
+
+//while loop
+$number = 10;
+ while($number<100){
+ 	echo "<br>".$number++;
+
+
+
+ }
+ // do while loop
+
+ $x = 1;
+ do{
+ 	echo "the number is:$x";
+ 	$x++;
+} while ($x < 10);
+
+//for loop
+
+for ($i=0; $i <10 ; $i++) { 
+	echo $i;
+}
+//for each loop
+
+  $colors = array("green","red","blue");
+  foreach ($colors as $value) {
+  	echo $value;
+  }
+
+
+
+
+
+
+
+
+?>
