e79fcf4c27102252d403defcfecad8565b3301a5
Your Name <you@example.com>
2017-04-05 11:54:37 -0700

    Selecting data from database

--- select.php
+++ select.php
@@ -11,10 +11,12 @@
 	<th>Student Name:</th>
 	<th>Father Name:</th>
 	<th>School Name</th>
+	<th>Roll Number</th>
 	<th>Result</th>
 </tr>
 
 <h1 align="center"><?php echo @$_Get['deleted']; ?> </h1>
+<h1 align="center"><?php echo @$_Get['updated']; ?> </h1>
 
 <?php
 $host='localhost';
@@ -38,6 +40,7 @@ if(!$con){
 		$name = $row['name'];
 		$f_name = $row['father_name'];
 		$school_name = $row['school_name'];
+		$roll_number = $row['roll_number'];
 		$result = $row['result'];
 	?>
 
@@ -47,6 +50,7 @@ if(!$con){
 	<td><?php echo $name; ?></td>
 	<td><?php echo $f_name; ?></td>
 	<td><?php echo $school_name; ?></td>
+	<td><?php echo $roll_number; ?></td>
 	<td><?php echo $result; ?></td>
 	<td><a href="delete.php?del=<?php echo $id; ?>">Delete</td>
 	<td><a href="edit.php?edit=<?php  echo $id; ?>">Edit</td>
