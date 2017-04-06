3debed4f6aedefbede8f85cdbe251e3a53382ff6
fahad611 <you@example.com>
2017-04-05 23:10:25 -0700

    Added files containing arrays code

--- arrays.php
+++ arrays.php
@@ -4,8 +4,4 @@ $info = array("fahad","age","email@gmail.com","karachi","paksitani","male");
 
 echo "Name of the student is:".$info[0]."<br> age of student:".$info[1]."<br>email id of student:".$info[2].    "<br> city of student:".$info[3]."<br> nationality of student:".$info[4]."<br>gender of student:".$info[5];
 
-
-
-
-
 ?>
