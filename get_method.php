8cab962d3fa1ea7dfcdd556f530b49f944434dde
fahad611 <you@example.com>
2017-04-06 12:43:40 -0700

    Get method in php

---
+++ get_method.php
@@ -0,0 +1,21 @@
+ <!DOCTYPE html>
+ 	<head>
+  	<title>php get and post</title>
+ 	</head>
+ <body>
+	<form align="center" action="get_post.php" method="get">
+	
+	username: <input type="text" name="name"></input><br><br>
+	Userrmail: <input type="text" name="email"></input><br>
+	<input type="submit" name="submit" value="Sign up">
+	
+	</form>
+ </body>
+ </html>
+ 
+ <?php
+	if(isset($_GET['submit'])) {
+ 	echo $name = $_GET['name'];
+ 	echo $email = $_GET ['email'];
+		}
+?>
