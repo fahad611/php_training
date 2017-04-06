d2478d99be5407b1b133f7a6d9c7402f4b9ed1e6
fahad611 <you@example.com>
2017-04-06 12:56:02 -0700

    PHP Loops

--- loops.php
+++ loops.php
@@ -1,38 +1,26 @@
 <?php
+	//while loop
+	$number = 10;
 
-//while loop
-$number = 10;
- while($number<100){
- 	echo "<br>".$number++;
-
-
-
- }
- // do while loop
-
- $x = 1;
- do{
- 	echo "the number is:$x";
+	 while($number<100){
+ 		echo "<br>".$number++;
+	  }
+ 		// do while loop
+	$x = 1;
+ 	do{
+ 		echo "the number is:$x";
  	$x++;
-} while ($x < 10);
 
-//for loop
+	} while ($x < 10);
 
-for ($i=0; $i <10 ; $i++) { 
-	echo $i;
-}
-//for each loop
+			//for loop
 
-  $colors = array("green","red","blue");
-  foreach ($colors as $value) {
-  	echo $value;
-  }
-
-
-
-
-
-
-
-
+	for ($i=0; $i <10 ; $i++) { 
+		echo $i;
+	}
+			//for each loop
+	$colors = array("green","red","blue");
+    foreach ($colors as $value) {
+  		echo $value;
+  	}
 ?>
