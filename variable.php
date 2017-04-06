d2954e0f8ec9f64c4f975fc8b5ba978f2fdc87bd
fahad611 <you@example.com>
2017-04-06 13:26:24 -0700

    Creating (Declaring) PHP Variables

--- variable.php
+++ variable.php
@@ -1,22 +1,17 @@
 <?php
- $name = "Fahad Rashid";
- $age = 24;
- $email = "asd@gmail.com";
+	 $name = "Fahad Rashid";
+	 $age = 24;
+	 $email = "asd@gmail.com";
+	 $result = "$name$age$email";
 
- $result = "$name$age$email";
+		 $age1 = 24;
+		 $age2 = 24;
 
- $age1 = 24;
- $age2 = 24;
+	$result1 = $age1 + $age2;
 
- 
+ 		echo "$result1<br>";
 
- $result1 = $age1 + $age2;
+ 		echo "$result<br>";
 
- echo "$result1<br>";
-
- echo "$result<br>";
-
- echo "your name is:".$name."<br>your age is:".$age."<br>yoir email id is:".$email;
-
-
+ 		echo "your name is:".$name."<br>your age is:".$age."<br>yoir email id is:".$email;
 ?>
