3cf2d680bfb29f71727744e86465f1733fa4a4cb
fahad611 <you@example.com>
2017-04-07 12:12:41 -0700

    Added file of php data types

--- data_types.php
+++ data_types.php
@@ -1,18 +1,25 @@
 <?php
-	//string data type
-	$name = "fahad";
-	
-	//integer data type
-	$number = 123;
 
-	//boolean data type
-	$value1 = true;
-	$value2 = false;
+// string data type
 
-	if ($value1 == false) {
-		echo "this is true";
-	}
-	else{
-		echo "this value is false";
-	}
+$name = "fahad";
+
+// integer data type
+
+$number = 123;
+
+// boolean data type
+
+$value1 = true;
+$value2 = false;
+
+if ($value1 == false)
+{
+	echo "this is true";
+}
+else
+{
+	echo "this value is false";
+}
+
 ?>
