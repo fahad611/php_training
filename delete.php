be0af7e4e66f2c90beac3f7761757c8e2453256a
fahad611 <you@example.com>
2017-04-07 12:16:43 -0700

    Delete data from mysql table using my mysqli

--- delete.php
+++ delete.php
@@ -1,16 +1,19 @@
 <?php
-		$host='localhost';
-		$user='root'; 
-		$pass='';
-		$db='school';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'school';
+$con = new mysqli($host, $user, $pass, $db);
 
-		$con=new mysqli($host,$user,$pass,$db);
+if (!$con)
+{
+	die("connnection failed:" . mysqli_connect_error());
+}
 
-		if (!$con) {
-		die("connnection failed:" .mysqli_connect_error());
-			}
-		$delete_id = $_GET['del'];
-		$query = "DELETE FROM students where id='$delete_id'"; 
-		if(mysqli_query($con,$query)){
-		echo "<script>window.open('select.php?deleted = data has been deleted...','_self')</script>";
+$delete_id = $_GET['del'];
+$query = "DELETE FROM students where id='$delete_id'";
+
+if (mysqli_query($con, $query))
+{
+	echo "<script>window.open('select.php?deleted = data has been deleted...','_self')</script>";
 ?>
