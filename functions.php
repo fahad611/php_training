b5c53753b5e517d150357791a31e565d627c27c5
fahad611 <you@example.com>
2017-04-07 12:27:59 -0700

    Create a User Defined Function

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
-	function userfun(){
-    echo $name = "fahad rashid<br>";
-    echo $age = 18;
-    echo $email = "fahadrashid@gmail.com<br>";
-	}
 
- 	userfun();// call the function
+function userfun()
+{
+	echo $name = "fahad rashid<br />";
+	echo $age = 18;
+	echo $email = "fahadrashid@gmail.com<br />";
+}
+
+userfun(); // call the function
+
 ?>
