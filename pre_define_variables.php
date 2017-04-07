e956ce736341d0ff2b1cb77e8dd4d856e2f0185b
fahad611 <you@example.com>
2017-04-07 12:43:28 -0700

    PHP Global Variables - Superglobals

--- pre_define_variables.php
+++ pre_define_variables.php
@@ -1,68 +1,71 @@
 <?php
 
-	//Global Variables
+// Global Variables
 
-		$x = 75; 
-		$y = 25;
- 
-	function addition() { 
-    	$GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y']; 
+$x = 75;
+$y = 25;
+
+function addition()
+{
+	$GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+}
+
+addition();
+echo $z;
+
+// PHP $_SERVER
+
+echo $_SERVER['PHP_SELF'];
+echo "<br />";
+echo $_SERVER['SERVER_NAME'];
+echo "<br />";
+echo $_SERVER['HTTP_HOST'];
+echo "<br />";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br />";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br />";
+echo $_SERVER['SCRIPT_NAME'];
+
+// PHP $_REQUEST
+
+ < formmethod = "post"action = "<?php echo $_SERVER['PHP_SELF'];?>" > Name: < inputtype = "text"name = "fname" > < inputtype = "submit" > < / form >
+	if ($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+
+		// collect value of input field
+
+		$name = $_REQUEST['fname'];
+		if (empty($name))
+		{
+			echo "Name is empty";
+		}
+		else
+		{
+			echo $name;
+		}
 	}
- 
-	addition(); 
-		echo $z; 
-       
-    //PHP $_SERVER
 
-	echo $_SERVER['PHP_SELF'];
-	echo "<br>";
-	echo $_SERVER['SERVER_NAME'];
-	echo "<br>";
-	echo $_SERVER['HTTP_HOST'];
-	echo "<br>";
-	echo $_SERVER['HTTP_REFERER'];
-	echo "<br>";
-	echo $_SERVER['HTTP_USER_AGENT'];
-	echo "<br>";
-	echo $_SERVER['SCRIPT_NAME'];
+	// PHP $_POST
 
-	//PHP $_REQUEST
+	 < formmethod = "post"action = "<?php echo $_SERVER['PHP_SELF'];?>" > Name: < inputtype = "text"name = "fname" > < inputtype = "submit" > < / form > < ? php
+		if ($_SERVER["REQUEST_METHOD"] == "POST")
+		{
 
-	<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  	Name: <input type="text" name="fname">
-	<input type="submit">
-	</form>
+			// collect value of input field
 
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    	// collect value of input field
-    	$name = $_REQUEST['fname'];
-    	if (empty($name)) {
-        echo "Name is empty";
-    	} else {
-        echo $name;
-    		}
-		}
-	//PHP $_POST
-
-	<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-	 Name: <input type="text" name="fname">
-	 <input type="submit">
-	
-	</form>
-
-<?php
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	    // collect value of input field
-	    $name = $_POST['fname'];
-	    if (empty($name)) {
-		echo "Name is empty";
-	    } else {
-		echo $name;
-	    	}
+			$name = $_POST['fname'];
+			if (empty($name))
+			{
+				echo "Name is empty";
+			}
+			else
+			{
+				echo $name;
+			}
 		}
 
-		//PHP $_GET
-<?php 
-	echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
+		// PHP $_GET
+
+		 < ? phpecho "Study " . $_GET['subject'] . " at " . $_GET['web'];
 ?>
-	
